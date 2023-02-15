5ef14625fcdc3e94d57843b5cfa0729df9960828
Nguyễn Gia Phong <mcsinyx@disroot.org>
2023-02-16 02:24:28 +0900

    mpg123: add lib-only derivation not depending on sound servers

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -31841,6 +31841,11 @@ with pkgs;
     jack = libjack2;
   };
 
+  libmpg123 = mpg123.override {
+    libOnly = true;
+    withConplay = false;
+  };
+
   mpg321 = callPackage ../applications/audio/mpg321 { };
 
   mpc-cli = callPackage ../applications/audio/mpc {
